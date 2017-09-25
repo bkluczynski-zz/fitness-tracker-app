d8a54153a25ce15642a91891b10a227e0745e32d
Bart <kluczynskib@gmail.com>
2017-09-25 12:07:41 +0100

    run create-react-native and tested ios simulator

---
+++ App.js
@@ -0,0 +1,25 @@
+<script src="http://localhost:8097"></script>
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>how are you?</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
