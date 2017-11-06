dfe0ca7ad20c1d5235b49922389dac7ccb3f9388
Bart <kluczynskib@gmail.com>
2017-11-06 11:43:35 +0000

    added TabNavigator to app component

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Platform, TouchableOpacity, StatusBar } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
-import reducer from './reducers'
-import History from './components/History'
+import reducer from './reducers';
+import History from './components/History';
+import { TabNavigator } from 'react-navigation';
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
 
+function UdaciStatusBar({backgroundColor, ...props}){
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+
+const Tabs = TabNavigator({
+  History: {
+    screen : History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen : AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
 
@@ -13,8 +60,8 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}} >
-          <View style={{height: 20}} />
-          <History />
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
+          <Tabs />
         </View>
       </Provider>
     );
--- components/MetricCard.js
+++ components/MetricCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import DateHeader from './DateHeader'
 import { getMetricMetaInfo } from '../utils/helpers'
-import { gray } from '../utils/colors'
+import { grey } from '../utils/colors'
 
 export default function MetricCard({date, metrics}) {
   return (
